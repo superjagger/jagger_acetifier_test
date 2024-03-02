0da5b46deb31702421a49e1787a2ee3b77c38121
superjagger <31763819+superjagger@users.noreply.github.com>
2024-03-02 21:31:00 +0800

    Add files via upload

---
+++ lib/jagger_acetifier_test.rb
@@ -0,0 +1,6 @@
+require "jagger_acetifier_test/version"
+
+module JaggerAcetifierTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_acetifier_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerAcetifierTest
+  VERSION = "0.1.0"
+end
